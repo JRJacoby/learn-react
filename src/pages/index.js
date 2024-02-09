6666a6d41b679a629cc7c096fcbc8378775dbd8a
John <jrdjacoby@gmail.com>
2024-02-09 16:25:43 -0500

    exercises

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,12 +2,16 @@
 // import Image from 'next/image'
 // import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
-import Profile from './components/profile'
+import Gallery from './components/gallery'
+import Profile from './qcomps/profile_mistake.js'
+import MyComp from './qcomps/firstcomp.js'
+import Bio from './qcomps/bios.js'
+import TodoList from './qcomps/todos.js'
 
 export default function Home() {
   return (
     <div className={styles.main}>
-        <Profile />
+        <TodoList />
     </div>
   )
 }
