774db9d0f9a23624d8e208cb4e26aa7f32cc62e9
John <jrdjacoby@gmail.com>
2024-02-16 16:36:26 -0500

    stuckForm.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,11 +9,12 @@ import Bio from './qcomps/bios.js'
 import TodoList from './qcomps/todos.js'
 import List from './qcomps/list_keys_id.js'
 import RecipeList from './qcomps/recipes.js'
+import Form from './qcomps/stuckForm.js'
 
 export default function Home() {
   return (
     <div className={styles.main}>
-        <Gallery />
+        <Form />
     </div>
   )
 }
